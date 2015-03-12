c24c5ec947749d595f5e0bec3a795bd6b301d014
aladinbouzerd <aladin.bouzerd@gmail.com>
2015-03-12 13:58:14 +0800

    Update MenuItem.php

--- MenuItem.php
+++ MenuItem.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\HTML;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Contracts\ArrayableInterface;
+use Illuminate\Support\Contracts\Arrayable;
 
-class MenuItem implements ArrayableInterface {
+class MenuItem implements Arrayable {
     
     /**
      * Array properties.
